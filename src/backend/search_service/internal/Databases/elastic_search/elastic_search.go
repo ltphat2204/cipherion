e3971ffffeb68b45f1d1248d97a4c1e597cd8840
phonghocode <ntp11892vt@gmail.com>
2024-08-09 00:55:26 +0700

    update search service

--- src/backend/search_service/internal/Databases/elastic_search/elastic_search.go
+++ src/backend/search_service/internal/Databases/elastic_search/elastic_search.go
@@ -1,12 +1,21 @@
 package elastic_search
 
 import (
+	"fmt"
 	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
 )
 
 func Connect() (*elasticsearch.TypedClient, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	elasticURL := os.Getenv("ELASTIC_URL")
 	es, err := elasticsearch.NewTypedClient(elasticsearch.Config{
-		Addresses: []string{"http://localhost:9200"},
+		Addresses: []string{fmt.Sprintf("http://%s:9200", elasticURL)},
 	})
 
 	return es, err
