9bbc4b81b4c303056067c44d482d87cd780dcad7
Trần Nguyễn Hoàng Diễn <116612379+Diendien1801@users.noreply.github.com>
2024-08-18 16:45:38 +0700

    update final
    add upload track, searching, playlist and artist Controller

---
+++ src/frontend/lib/features/music/controllers/fav_playlist_controller.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_rx/src/rx_types/rx_types.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:lyrica_ver2/data/models/playlist_model.dart';
+import 'package:lyrica_ver2/data/models/song_model.dart';
+import 'package:lyrica_ver2/data/repositories/authentication.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class FavPlaylistController extends GetxController {
+  
+
+}
